312e328bf7d25d4a6dfa8c4a96ffee7077fc1a62
Denis Kalkopf <ddenproton@protonmail.com>
2025-02-21 18:15:22 +0500

    feat: add hero image

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div className="max-w-7xl mx-auto p-2">Main App</div>;
+  return (
+    <div>
+      <Image
+        className="object-cover h-[300px]"
+        alt=""
+        src="/hero-image.jpeg"
+        width={1920}
+        height={300}
+      />
+      <div className="max-w-7xl mx-auto p-2">Main App</div>
+    </div>
+  );
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header className="bg-neutral-600 text-neutral-50">
-      <div className="max-w-7xl mx-auto py-4 px-2">
+    <header className="bg-neutral-800 text-neutral-50">
+      <div className="max-w-7xl mx-auto p-2">
         <p className="font-semibold text-lg">Weather App</p>
       </div>
     </header>
